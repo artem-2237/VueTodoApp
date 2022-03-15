611634cc020e190b06621285ac62415fe75f89b0
artem-2237 <valera8461@gmail.com>
2022-03-15 13:22:12 +0200

    v1.0

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git init
 git add .
 git commit -m "deploy"
 
-git push -f git@github.com:artem-2237/todoApp.git master:gh-pages
+git push -f git@github.com:artem-2237/VueTodoApp.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: "/todoApp/"
+  publicPath: "/VueTodoApp/"
 }
